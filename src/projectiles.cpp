0162ecd545f429dfe875451684849f5ce52b1a2f
Konrad Kreczko <konradkreczko@gmail.com>
2026-04-05 09:50:34 +0200

    Added projectiles

---
+++ src/projectiles.cpp
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include "tuple.h"
+
+struct Projectile {
+    Point m_position;
+    Vector m_velocity;
+
+    Projectile(const Point &position, const Vector &velocity) 
+                : m_position(position), m_velocity(velocity) {}
+};
+
+struct Environment {
+    Vector m_gravity;
+    Vector m_wind;
+
+    Environment(const Vector &gravity, const Vector &wind) 
+                : m_gravity(gravity), m_wind(wind) {}
+};
+
+Projectile tick(const Projectile &before, const Environment &env) {
+    Point newPosition = before.m_position + before.m_velocity;
+    Vector newVelocity = before.m_velocity + env.m_gravity + env.m_wind;
+    return {newPosition, newVelocity};
+}
+
+int main() {
+    Projectile proj = Projectile(Point(0, 1, 0), Vector(1, 1, 0).normalize());
+    Environment env = Environment(Vector(0, -0.1, 0), Vector(-0.01, 0, 0));
+
+    while (!(proj.m_position.m_y <= 0)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::cout << "\r\033[K" << proj.m_position << std::flush;
+        proj = tick(proj, env);
+    }
+
+    std::cout << std::endl;
+}
--- src/tuple.h
+++ src/tuple.h
@@ -2,6 +2,7 @@
 #define TUPLE_H
 
 #include <cmath>
+#include <iostream>
 
 constexpr double EPSILON = 1e-10;
 
@@ -35,14 +36,22 @@ struct Tuple {
   Tuple &normalize();
 
   static double dot(const Tuple &lhs, const Tuple &rhs);
+
+  friend std::ostream& operator<<(std::ostream& os, const Tuple &t) {
+    os << "x = " << t.m_x << ", y = " << t.m_y << ", z = " << t.m_z << ", w = " << t.m_w;
+    return os;
+  };
+
 };
 
 struct Point : Tuple {
   Point(double x, double y, double z) : Tuple(x, y, z, 1.0) {}
+  Point(const Tuple &t) : Tuple(t.m_x, t.m_y, t.m_z, t.m_w) {}
 };
 
 struct Vector : Tuple {
   Vector(double x, double y, double z) : Tuple(x, y, z, 0.0) {}
+  Vector(const Tuple &t) : Tuple(t.m_x, t.m_y, t.m_z, t.m_w) {}
 
   static Vector cross(const Vector &lhs, const Vector &rhs);
 };
