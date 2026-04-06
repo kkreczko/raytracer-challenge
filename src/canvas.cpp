30b920766a09843c7df032f72a5d060bd1191cac
Konrad Kreczko <konradkreczko@gmail.com>
2026-04-07 00:34:30 +0200

    canvas to ppm

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -1,17 +1,35 @@
 #include "canvas.h"
 
+int clamp(double value) {
+    if (value < 0) return 0;
+    if (value > 1.0) return 255;
+    return static_cast<int>(std::round(value * 255));
+}
+
 Color &Canvas::getPixelAt(const int width, const int height) {
-    return m_pixels[width][height];
+    return m_pixels[height][width];
 }
 
 void Canvas::writePixelAt(const Color &pixel, const int width, const int height) {
-    m_pixels[width][height] = pixel;
+    m_pixels[height][width] = pixel;
 }
 
 void Canvas::writeAllPixelsTo(const Color &pixel) {}
 
 std::string Canvas::toPPM() const {
-    
+    std::ostringstream oss;
+    oss << "P3\n" << m_width << " " << m_height << "\n255\n";
+    for (int y = 0; y < m_height; y++) {
+        for (int x = 0; x < m_width; x++) {
+            const Color& c = m_pixels[y][x];
+            oss << clamp(c.red()) << " "
+                << clamp(c.green()) << " "
+                << clamp(c.blue());
+            if (x < m_width - 1) oss << " ";
+        }
+        oss << "\n";
+    }
+    return oss.str();
 }
 
 void Canvas::saveToFile(const std::string& filename) const {
--- src/canvas.h
+++ src/canvas.h
@@ -4,18 +4,15 @@
 #include <tuple.h>
 #include <vector>
 #include <fstream>
+#include <sstream>
 
-int clamp(double value) {
-    if (value < 0) return 0;
-    if (value > 1.0) return 255;
-    return static_cast<int>(std::round(value * 255));
-}
+int clamp(double value);
 
 struct Canvas {
     int m_width, m_height;
     std::vector<std::vector<Color>> m_pixels;
     Canvas(int width, int height) : m_width(width), m_height(height), 
-                                    m_pixels(m_width, std::vector<Color>(m_height, Color(0, 0, 0))) {}
+                                    m_pixels(m_height, std::vector<Color>(m_width, Color(0, 0, 0))) {}
     
     Color &getPixelAt(const int width, const int height);
     void writePixelAt(const Color &pixel, const int width, const int height);
