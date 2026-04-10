b77c9434118bc3e1538e0b12959322c148d0d9cd
Kreczko, Konrad <konrad.kreczko@intel.com>
2026-04-10 14:20:16 +0200

    Changed bracket include to quote include in canvas

--- src/canvas.h
+++ src/canvas.h
@@ -1,26 +1,27 @@
 #ifndef CANVAS_H
 #define CANVAS_H
 
-#include <tuple.h>
-#include <vector>
+#include "tuple.h"
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 int clamp(double value);
 
 struct Canvas {
-    int m_width, m_height;
-    std::vector<std::vector<Color>> m_pixels;
-    Canvas(int width, int height) : m_width(width), m_height(height), 
-                                    m_pixels(m_height, std::vector<Color>(m_width, Color(0, 0, 0))) {}
-    
-    Color &getPixelAt(int x, int y);
-    const Color& getPixelAt(int x, int y) const;
-    void writePixelAt(const Color &pixel, int x, int y);
-    void writeAllPixelsTo(const Color &pixel);
+  int m_width, m_height;
+  std::vector<std::vector<Color>> m_pixels;
+  Canvas(int width, int height)
+      : m_width(width), m_height(height),
+        m_pixels(m_height, std::vector<Color>(m_width, Color(0, 0, 0))) {}
 
-    std::string toPPM() const;
-    void saveToFile(const std::string& filename) const;
+  Color &getPixelAt(int x, int y);
+  const Color &getPixelAt(int x, int y) const;
+  void writePixelAt(const Color &pixel, int x, int y);
+  void writeAllPixelsTo(const Color &pixel);
+
+  std::string toPPM() const;
+  void saveToFile(const std::string &filename) const;
 };
 
 #endif
