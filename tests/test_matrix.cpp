5155f54b960b1caeb0fe4e29411cfd1b7aacb387
Konrad Kreczko <konradkreczko@gmail.com>
2026-04-29 23:48:47 +0200

    Added inverse tests.

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -198,3 +198,20 @@ TEST(Matrix, inverseBasic) {
                                     -0.52256, -0.81391, -0.30075, 0.30639}));
     EXPECT_TRUE(B == expected_inverted);
 }
+
+TEST(Matrix, inverseSecond) {
+    Matrix A = Matrix(4, 4, std::vector<double>({8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4}));
+    Matrix B = A.inverse();
+    Matrix expected = Matrix(4, 4, std::vector<double>({-0.15385, -0.15385, -0.28205, -0.53846, -0.07692, 0.12308, 0.02564, 0.03077, 0.35897, 0.35897, 0.43590, 0.92308, -0.69231, -0.69231, -0.76923, -1.92308}));
+    EXPECT_TRUE(B == expected);
+}
+
+TEST(Matrix, inverseThird) {
+    Matrix A = Matrix(4, 4, std::vector<double>({9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2}));
+    Matrix B = A.inverse();
+    Matrix expected = Matrix(4, 4, std::vector<double>({-0.04074, -0.07778, 0.14444, -0.22222,
+                                                         -0.07778, 0.03333, 0.36667, -0.33333,
+                                                         -0.02901, -0.14630, -0.10926, 0.12963,
+                                                         0.17778, 0.06667, -0.26667, 0.33333}));
+    EXPECT_TRUE(B == expected);
+}
